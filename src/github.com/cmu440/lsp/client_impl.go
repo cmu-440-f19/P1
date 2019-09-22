82e388b70dad8494a4e95fb7a016bf9eff5f7cd9
Ethan Yang <xuanyueyang@gmail.com>
2019-09-22 16:22:49 -0400

    add api comments in *_impl.go

--- src/github.com/cmu440/lsp/client_impl.go
+++ src/github.com/cmu440/lsp/client_impl.go
@@ -22,20 +22,39 @@ func NewClient(hostport string, params *Params) (Client, error) {
 	return nil, errors.New("not yet implemented")
 }
 
+// ConnID returns the connection ID associated with this client.
 func (c *client) ConnID() int {
 	return -1
 }
 
+// Read reads a data message from the server and returns its payload.
+// This method should block until data has been received from the server and
+// is ready to be returned. It should return a non-nil error if either
+// (1) the connection has been explicitly closed, (2) the connection has
+// been lost due to an epoch timeout and no other messages are waiting to be
+// returned, or (3) the server is closed. Note that in the third case, it is
+// also ok for Read to never return anything.
 func (c *client) Read() ([]byte, error) {
 	// TODO: remove this line when you are ready to begin implementing this method.
 	select {} // Blocks indefinitely.
 	return nil, errors.New("not yet implemented")
 }
 
+// Write sends a data message with the specified payload to the server.
+// This method should NOT block, and should return a non-nil error
+// if the connection with the server has been lost. If Close has been called on
+// the client, subsequent calls to Write must either return a non-nil error, or
+// never return anything.
 func (c *client) Write(payload []byte) error {
 	return errors.New("not yet implemented")
 }
 
+// Close terminates the client's connection with the server. It should block
+// until all pending messages to the server have been sent and acknowledged.
+// Once it returns, all goroutines running in the background should exit.
+//
+// Note that after Close is called, further calls to Read, Write, and Close
+// must either return a non-nil error, or never return anything.
 func (c *client) Close() error {
 	return errors.New("not yet implemented")
 }
--- src/github.com/cmu440/lsp/server_impl.go
+++ src/github.com/cmu440/lsp/server_impl.go
@@ -18,20 +18,49 @@ func NewServer(port int, params *Params) (Server, error) {
 	return nil, errors.New("not yet implemented")
 }
 
+
+// Read reads a data message from a client and returns its payload,
+// and the connection ID associated with the client that sent the message.
+// This method should block until data has been received from some client.
+// It should return a non-nil error if either (1) the connection to some
+// client has been explicitly closed, (2) the connection to some client
+// has been lost due to an epoch timeout and no other messages from that
+// client are waiting to be returned, or (3) Close() has been called on the server.
+// In the first two cases, the client's connection ID and a non-nil
+// error should be returned. In the third case, an ID with value 0 and
+// a non-nil error should be returned. Note that in the third case,
+// it is also ok for Read to never return anything.
 func (s *server) Read() (int, []byte, error) {
 	// TODO: remove this line when you are ready to begin implementing this method.
 	select {} // Blocks indefinitely.
 	return -1, nil, errors.New("not yet implemented")
 }
 
+
+// Write sends a data message to the client with the specified connection ID.
+// This method should NOT block, and should return a non-nil error if the
+// connection with the client has been lost. If Close has been called on the server,
+// subsequent calls to Write must either return a non-nil error, or never return anything.
 func (s *server) Write(connId int, payload []byte) error {
 	return errors.New("not yet implemented")
 }
 
+// CloseConn terminates the client with the specified connection ID, returning
+// a non-nil error if the specified connection ID does not exist. All pending
+// messages to the client should be sent and acknowledged. However, unlike Close,
+// this method should NOT block.
 func (s *server) CloseConn(connId int) error {
 	return errors.New("not yet implemented")
 }
 
+// Close terminates all currently connected clients and shuts down the LSP server.
+// This method should block until all pending messages for each client are sent
+// and acknowledged. If one or more clients are lost during this time, a non-nil
+// error should be returned. Once it returns, all goroutines running in the
+// background should exit.
+//
+// Note that after Close is called, further calls to Read, Write, CloseConn, and Close
+// must either return a non-nil error, or never return anything.
 func (s *server) Close() error {
 	return errors.New("not yet implemented")
 }
